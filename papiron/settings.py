456345a7c5f634e45a6e80bfdb934d11339d7c81
ablitzkow <blitzkow@gmail.com>
2022-09-17 11:41:54 -0300

    css03

--- papiron/settings.py
+++ papiron/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATIC_URL = '/home/blitzkow/blitzkow.pythonanywhere.com/papiron/'
+STATIC_URL = '/home/blitzkow/blitzkow.pythonanywhere.com/papiron/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'papiron/static')
 ]

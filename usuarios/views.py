97ace661da1ebc6ecd6ecef8ae63e5c905001361
ablitzkow <blitzkow@gmail.com>
2022-09-17 23:00:33 -0300

    32

--- usuarios/views.py
+++ usuarios/views.py
@@ -358,7 +358,7 @@ def form_dados(request):
                 # name_file = request.POST['cpf'].replace(".","").replace("-","")
 
                 a = os.path.abspath(os.getcwd())
-                path_media = os.path.join(a, 'blitzkow.pythonanywhere.com/media/usuarios/')
+                path_media = os.path.join(a, '/media/usuarios/')
                 # path_a = os.path.join(a, '/media/usuarios/')
                 # path_b = os.path.join(b, '/media/usuarios/')
                 # print("PATH_MEDIA",path_media,path_a,path_b)

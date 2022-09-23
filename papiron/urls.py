44e6a89ff6dad6a462a50bea573d00bac8e803a2
ablitzkow <blitzkow@gmail.com>
2022-09-23 09:46:56 -0300

    sp03

--- papiron/urls.py
+++ papiron/urls.py
@@ -20,3 +20,5 @@ urlpatterns = [
 from django.contrib.sitemaps.views import sitemap
 path('sitemap.xml', sitemap, {'sitemaps': 'sitemaps'},
      name='django.contrib.sitemaps.views.sitemap')
+path('sitemap2.xml', sitemap, {'sitemaps': 'sitemaps'},
+     name='django.contrib.sitemaps.views.sitemap')

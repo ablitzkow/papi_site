4696fb009aa45d346f83473be90ceb89d2bfa146
ablitzkow <blitzkow@gmail.com>
2022-09-25 11:44:16 -0300

    as01

--- papiron/urls.py
+++ papiron/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('', views.index , name='index'),
     path('perguntas/', include('perguntas.urls')), 
     path('usuarios/', include('usuarios.urls')),
+    path('sm/sitemap',views.sitemap,name='sitemap'),
     path('reset_password/', auth_views.PasswordResetView.as_view(template_name="usuarios/password/reset_senha.html"), name="reset_password"),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="usuarios/password/reset_senha_envio.html"), name="password_reset_done"),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="usuarios/password/reset_senha_confirmacao.html"), name="password_reset_confirm"),
--- papiron/views.py
+++ papiron/views.py
@@ -1,6 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 def index(request):
     
     return render(request,'index.html')
 
+def sitemap(request):
+    return HttpResponse(open('sitemap.xml').read(), content_type='text/xml')
+    # return render (request,"sitemap.xml")

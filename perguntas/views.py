23d730ed64df3dffac2a90659046e3b77e778af6
ablitzkow <blitzkow@gmail.com>
2022-09-27 09:26:18 -0300

    270922_02 - rq auto

--- perguntas/views.py
+++ perguntas/views.py
@@ -187,5 +187,5 @@ def auto_publicar(request):
     from django.http import HttpResponse
     from datetime import datetime, timedelta
     data = datetime.today()-timedelta(days=7)
-    Pergunta.objects.order_by('-data').filter(data__gte=data,publicada=False).update(publicada=True)
+    Pergunta.objects.order_by('-data').filter(data__lte=data,publicada=False).update(publicada=True)
     return HttpResponse(status=201)

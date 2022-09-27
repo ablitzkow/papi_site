3fe29b469f891dc9033f63526fdba3242cf67a78
ablitzkow <blitzkow@gmail.com>
2022-09-27 20:16:08 -0300

    270922_07-sm2.py

--- perguntas/publicacao.py
+++ perguntas/publicacao.py
@@ -1,3 +1,6 @@
 import requests as rq
 url='https://www.papiron.com.br/perguntas/robot/w4Rt5i7pw/auto_publicar'
+url1 = 'https://www.papiron.com.br/robot/Yudo7wmr4/gera_sitemap'
 rq.get(url)
+rq.get(url1)
+

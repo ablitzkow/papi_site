accb23baf6525c064ac1f1f1bc335f552c6079e2
ablitzkow <blitzkow@gmail.com>
2022-09-27 10:01:46 -0300

    270922_04- rq auto url

--- perguntas/publicacao.py
+++ perguntas/publicacao.py
@@ -1,11 +1,3 @@
 import requests as rq
-
-# url = 'https://www.papiron.com.br/usuarios/login?action=logar'
-# rq = rq.get(url)
-# crsftoken = rq.headers['Set-Cookie'].split(';').__getitem__(0)
-# user = 'blitzkow@gmail.com'
-
-
-url='https://www.papiron.com.br/robot/w4Rt5i7pw/auto_publicar'
-
+url='https://www.papiron.com.br/perguntas/robot/w4Rt5i7pw/auto_publicar'
 rq.get(url)

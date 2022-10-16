286211382f9b976cb06f06c30075d82648f07261
ablitzkow <blitzkow@gmail.com>
2022-10-16 16:19:54 -0300

    20221016_04 robots.txt stats whats

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('form_incluir_assinante',forms.form_incluir_assinante, name = 'form_incluir_assinante'),
     path('captcha', forms.captcha, name = 'captcha'),
     path('whatsapp', views.whatsapp, name = 'whatsapp'),
+    path('stats/whatsapp', views.whatsapp, name = 'whatsapp'),
     path('stats_whatsapp', views.stats_whatsapp, name = 'stats_whatsapp'),
 
 ]
